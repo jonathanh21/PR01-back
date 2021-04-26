77e078add5e3519842ac8d8180f01f840b34a2c4
jonathanh21 <jhonathan.herrerao@gmail.com>
2021-04-25 20:54:39 -0500

    Se agrega filtro por ID y plantilla 404

--- app.js
+++ app.js
@@ -7,15 +7,23 @@ const http = require('http')
 http.createServer((req, res)=> {
     const url = req.url
 
-    switch(url){
-        case "/api":
-            const api = fs.readFileSync(__dirname + '/data.json')
-            res.writeHead(200, {'Content-Type': 'application/json'})
-            res.end(api)
-            return
-        default:
-            break //404 here!!!!
+    if(url==="/api"){
+        const api = fs.readFileSync(__dirname + '/data.json')
+        res.writeHead(200, {'Content-Type': 'application/json'})
+        res.end(api)
+        return
     }
-    
-    res.end()
+    if (url.substring(0,5) === "/api/" && url.length > 5){
+        const dir = decodeURI(url.split('/')[2])
+        const api = fs.readFileSync(__dirname + '/data.json')
+        const char = JSON.parse(api).filter(char => char.id == dir)
+        console.log(JSON.stringify(char))
+        res.writeHead(200,{'Content-Type': 'application/json'})
+        res.end(JSON.stringify(char))
+        return
+    }
+    const err = fs.readFileSync(__dirname + '/public/error.html')
+    res.writeHead(404,{'Content-Type': 'text/html'})
+    res.end(err)
+
 }).listen(3001, '127.0.0.1')
